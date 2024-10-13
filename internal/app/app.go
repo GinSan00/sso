5dc8c9ea41285091b6dcf0fbf52f7f6e98ca3133
GinSan <eliseevm553@mail.ru>
2024-10-14 00:10:09 +0500

    first commit

---
+++ internal/app/app.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"log/slog"
+	"time"
+
+	grpcapp "sso-service-grpc/internal/app/grpc"
+
+	"sso-service-grpc/internal/services/auth"
+	"sso-service-grpc/internal/storage/sqlite"
+)
+
+type App struct {
+	GRPCSrv *grpcapp.App
+}
+
+func New(
+	log *slog.Logger,
+	grpcPort int,
+	storagePath string,
+	tokenTTL time.Duration,
+) *App {
+
+	storage, err := sqlite.New(storagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+
+	grpcApp := grpcapp.New(log, authService, grpcPort)
+
+	return &App{
+		GRPCSrv: grpcApp,
+	}
+}
